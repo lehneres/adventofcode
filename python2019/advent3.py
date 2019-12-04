5dd2325fe6f6e6d218b9dc405e23c44b44e66243
lehneres <lehneres@outlook.com>
2019-12-04 07:55:05 +0100

    wip

--- python2019/advent3.py
+++ python2019/advent3.py
@@ -4,10 +4,12 @@ from shapely.geometry import LineString, Point
 
 def distance(cables):
     paths = []
+    steps = []
     for cable in cables:
-        path = [(0, 0)]
+        path = [((0, 0), 0)]
         for insts in cable.split(','):
-            x, y = path[-1]
+            coord, steps = path[-1]
+            x, y = coord
             if insts[0] == 'U':
                 y += int(insts[1:])
             elif insts[0] == 'D':
@@ -18,25 +20,21 @@ def distance(cables):
                 x -= int(insts[1:])
             else:
                 raise Exception('unknown direction')
-            path.append((x, y))
+            path.append(((x, y), steps + abs(int(insts[1:]))))
         paths.append(path)
     intersections = []
     for opath in paths:
         for o in range(len(opath) - 1):
-            oline = LineString([opath[o], opath[o + 1]])
+            oline = LineString([opath[o][0], opath[o + 1][0]])
             for ipath in paths:
                 if opath == ipath:
                     continue
                 for i in range(len(ipath) - 1):
-                    iline = LineString([ipath[i], ipath[i + 1]])
+                    iline = LineString([ipath[i][0], ipath[i + 1][0]])
                     intersection = oline.intersection(iline)
                     if not (intersection.equals(Point((0, 0))) or intersection.is_empty):
                         intersections.append((intersection.x, intersection.y))
-    intersections = set(intersections)
-    distances = []
-    for i in intersections:
-        distances.append(abs(i[0] - 0) + abs(i[1] - 0))
-    return min(distances)
+    return min([abs(i[0] - 0) + abs(i[1] - 0) for i in set(intersections)])
 
 
 if __name__ == '__main__':
