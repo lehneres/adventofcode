e1823e1467295bd73d345efbbf1bc548b6cfe6c0
lehneres <lehneres@outlook.com>
2019-12-07 11:12:11 +0100

    advent3 I & II

--- python2019/advent3.py
+++ python2019/advent3.py
@@ -1,43 +1,83 @@
 from aocd.models import Puzzle
-from shapely.geometry import LineString, Point
+from math import floor
+import numpy as np
+from scipy.spatial.distance import cdist
+from scipy.sparse import coo_matrix, find
+
+
+def get_dimensions(cable):
+    m_x, m_y = -1, -1
+    x, y = 0, 0
+    for point in cable.split(','):
+        if point[0] == 'U' or point[0] == 'D':
+            if abs(y + int(point[1:])) > abs(y):
+                m_y = y
+            y += int(point[1:])
+        elif point[0] == 'R' or point[0] == 'L':
+            if abs(x + int(point[1:])) > abs(x):
+                m_x = x
+            x += int(point[1:])
+    return max(m_x * 2, m_y * 2) + 1
 
 
 def distance(cables):
-    paths = []
-    steps = []
-    for cable in cables:
-        path = [((0, 0), 0)]
-        for insts in cable.split(','):
-            coord, steps = path[-1]
-            x, y = coord
-            if insts[0] == 'U':
-                y += int(insts[1:])
-            elif insts[0] == 'D':
-                y -= int(insts[1:])
-            elif insts[0] == 'R':
-                x += int(insts[1:])
-            elif insts[0] == 'L':
-                x -= int(insts[1:])
-            else:
-                raise Exception('unknown direction')
-            path.append(((x, y), steps + abs(int(insts[1:]))))
-        paths.append(path)
-    intersections = []
-    for opath in paths:
-        for o in range(len(opath) - 1):
-            oline = LineString([opath[o][0], opath[o + 1][0]])
-            for ipath in paths:
-                if opath == ipath:
-                    continue
-                for i in range(len(ipath) - 1):
-                    iline = LineString([ipath[i][0], ipath[i + 1][0]])
-                    intersection = oline.intersection(iline)
-                    if not (intersection.equals(Point((0, 0))) or intersection.is_empty):
-                        intersections.append((intersection.x, intersection.y))
-    return min([abs(i[0] - 0) + abs(i[1] - 0) for i in set(intersections)])
+    crossings, center, matrices = find_overlap(cables)
+    distances = cdist(np.array([[center, center]]), crossings, metric='cityblock')
+    return min(distances[0])
+
+
+def find_overlap(cables):
+    dim = max([get_dimensions(cable) for cable in cables])
+    matrices = [build_matrix(cable, dim) for cable in cables]
+    center = floor(dim / 2)
+    crossings = find(sum([m.sign() for m in matrices]) == 2)
+    crossings = np.stack((crossings[0], crossings[1]), axis=-1)
+    crossings = np.delete(crossings, (crossings == (center, center)).all(axis=1).nonzero(), 0)
+    return crossings, center, matrices
+
+
+def steps(cables):
+    crossings, center, matrices = find_overlap(cables)
+    total_steps = sum(matrices)
+    return min([total_steps[tuple(c)] for c in crossings])
+
+
+def build_matrix(cable, dim):
+    step = 0
+    x_coords = []
+    y_coords = []
+    data = []
+    x, y = floor(dim / 2), floor(dim / 2)
+    for point in cable.split(','):
+        if point[0] == 'U':
+            for i in range(y, y + int(point[1:])):
+                step = move(x, i, x_coords, y_coords, step, data)
+            y += int(point[1:])
+        elif point[0] == 'D':
+            for i in range(y, y - int(point[1:]), -1):
+                step = move(x, i, x_coords, y_coords, step, data)
+            y -= int(point[1:])
+        elif point[0] == 'R':
+            for i in range(x, x + int(point[1:])):
+                step = move(i, y, x_coords, y_coords, step, data)
+            x += int(point[1:])
+        elif point[0] == 'L':
+            for i in range(x, x - int(point[1:]), -1):
+                step = move(i, y, x_coords, y_coords, step, data)
+            x -= int(point[1:])
+    return coo_matrix((data, (y_coords, x_coords)), shape=(dim, dim))
+
+
+def move(x, y, x_coords, y_coords, step, data):
+    x_coords.append(x)
+    y_coords.append(y)
+    data.append(step)
+    step += 1
+    return step
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=2019, day=3)
     cables = list(puzzle.input_data.splitlines())
     puzzle.answer_a = int(distance(cables))
+    puzzle.answer_b = steps(cables)
--- python2019/test_advent3.py
+++ python2019/test_advent3.py
@@ -1,23 +1,26 @@
 import unittest
-from advent3 import distance
+from advent3 import distance, steps
 
 
+# noinspection PyMethodMayBeStatic
 class AdventTest(unittest.TestCase):
     def test_fist_1(self):
         data = ['R8,U5,L5,D3', 'U7,R6,D4,L4']
-        result = 6
-        assert result == distance(data)
+        true_distance = 6
+        true_steps = 30
+        assert true_distance == int(distance(data))
+        assert true_steps == steps(data)
 
     def test_fist_2(self):
         data = ['R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83']
-        result = 159
-        assert result == distance(data)
+        true_distance = 159
+        true_steps = 610
+        assert true_distance == int(distance(data))
+        assert true_steps == steps(data)
 
     def test_fist_3(self):
         data = ['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7']
-        result = 135
-        assert result == distance(data)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        true_distance = 135
+        true_steps = 410
+        assert true_distance == int(distance(data))
+        assert true_steps == steps(data)
